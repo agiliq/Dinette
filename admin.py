0000523d50d64f8ab9e366d0a789103fdce12bd1
Usware Technologies <github@uswaretech.com>
2009-11-13 18:27:10 +0530

    Moving from svn. The Svn history is not being exported to github. The last svn export is being sent wholesale to github.

---
+++ admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from dinette import models
+
+admin.site.register(models.SuperCategory)
+admin.site.register(models.Category)
+admin.site.register(models.Ftopics)
+admin.site.register(models.Reply)
+admin.site.register(models.DinetteUserProfile)
+admin.site.register(models.SiteConfig)
+
+
+
