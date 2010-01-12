1056bf4d31b96b1dc612692f9070920fd77dfc75
shabda <shabda@ad127118-9ec2-4c8a-8d99-4e47e8ea721e>
2010-01-12 14:48:52 +0000

    Set szie of forms
    git-svn-id: https://svn.uswaretech.com/forumproject@189 ad127118-9ec2-4c8a-8d99-4e47e8ea721e

--- dinette/forms.py
+++ dinette/forms.py
@@ -5,14 +5,16 @@ from dinette.models import Ftopics ,Reply
 
 #create a form from this Ftopics and use this when posting the a Topic
 class FtopicForm(ModelForm):
-      class Meta:
-            model = Ftopics
-            fields = ('subject', 'message','file' )
+    subject = forms.CharField(widget = forms.TextInput(attrs={"size":90}))
+    message = forms.CharField(widget = forms.Textarea(attrs={"cols":90, "rows":10}))
+    class Meta:
+        model = Ftopics
+        fields = ('subject', 'message','file' )
             
 
 #create a form from Reply
 class ReplyForm(ModelForm):
-    message = forms.CharField(widget = forms.Textarea(attrs={"cols":70, "rows":8}))
+    message = forms.CharField(widget = forms.Textarea(attrs={"cols":90, "rows":10}))
     class Meta:
         model = Reply
         fields = ('message','file')
