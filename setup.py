f919ee3754cad40100cef79384bce4ddc3b2b165
Javed Khan <javed@agiliq.com>
2010-08-09 20:03:37 +0530

    packaging stuff

--- setup.py
+++ setup.py
@@ -1,11 +1,35 @@
 import os
 from setuptools import setup, find_packages
 
-setup(name='dinette',
-      description='Dinette is a forum application in the spirit of PunBB.',
-      keywords='django, forum',
+overview = file('docs/overview.txt')
+data = overview.read()
+overview.close()
 
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
+setup(
+    name='dinette',
+    description='Dinette is a forum application in the spirit of PunBB.',
+    keywords='django, forum',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    version="1.1",
+    author="Agiliq Solutions",
+    author_email="hello@agiliq.com",
+    long_description= data,
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                   'Topic :: Internet :: WWW/HTTP :: WSGI',
+                   'Topic :: Software Development :: Libraries :: Application Frameworks',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+    url="http://www.agiliq.com/",
+    license="GPL",
+    platforms=["all"],
 )
