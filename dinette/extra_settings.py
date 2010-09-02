d96df637ced7fb54bad27e4267ff81dc36251472
Javed Khan <javed@agiliq.com>
2010-09-02 11:50:43 +0530

    updated dinette specific settings

--- dinette/extra_settings.py
+++ dinette/extra_settings.py
@@ -1,25 +1,27 @@
+# Dinette Settings
 import os
-from django.conf import global_settings
 
-
-#topic page size
 TOPIC_PAGE_SIZE = 3
 
-
-#REPLY PAGE SIZE
 REPLY_PAGE_SIZE = 3
-
-
-#Flood Time
-FLOOD_TIME = 5
-
-
-
 
 AUTH_PROFILE_MODULE = 'dinette.DinetteUserProfile'
 
-#Append out new template processors to already existing processors
-templist = list(global_settings.TEMPLATE_CONTEXT_PROCESSORS)
-templist.append("dinette.context_processors.get_announcement")
-templist.append("dinette.context_processors.get_site_config")
-TEMPLATE_CONTEXT_PROCESSORS = tuple(templist)
+RANKS_NAMES_DATA = ((30, "Member"), (100, "Senior Member"), (300, 'Star'))
+
+DINETTE_LOGIN_TEMPLATE = 'dinette/social_login.html'
+
+LOG_FILE_PATH = "\""+os.path.join(os.path.join(os.path.dirname(os.path.normpath(__file__)),'logs'),"logs.txt")+"\""
+LOG_FILE_PATH2 = "\""+os.path.join(os.path.join(os.path.dirname(os.path.normpath(__file__)),'logs2'),"logs2.txt")+"\""
+
+LOG_FILE_NAME =  os.path.join(os.path.dirname(os.path.normpath(__file__)),'logging.conf')
+
+FLOOD_TIME = 1000
+
+HAYSTACK_SITECONF = "dinette.search"
+
+HAYSTACK_SEARCH_ENGINE = 'whoosh'
+
+HAYSTACK_WHOOSH_PATH = os.path.join(os.path.dirname(os.path.normpath(__file__)),'index.db')
+
+SITE_URL = "http://127.0.0.1:8000"
