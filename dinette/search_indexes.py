9320f27da3bf41bde12f366b4e62fcf0cebed4e2
Javed Khan <javed@agiliq.com>
2011-03-11 17:53:28 +0530

    fixes #66: indenx rendered message

--- dinette/search_indexes.py
+++ dinette/search_indexes.py
@@ -6,11 +6,11 @@ from dinette.models import Ftopics, Reply, DinetteUserProfile
 class TopicIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     subject = indexes.CharField(model_attr="subject")
-    message = indexes.CharField(model_attr="message")
+    message = indexes.CharField(model_attr="_message_rendered")
     
 class ReplyIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
-    message = indexes.CharField(model_attr="message")
+    message = indexes.CharField(model_attr="_message_rendered")
     
 class UserprofileIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
