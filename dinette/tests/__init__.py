653f66bf8133ffd706c91313edb1aeb1d1aa9346
Plaban <plaban.nayak@gmail.com>
2011-09-29 12:17:55 +0530

    some more tests added to dinette

--- dinette/tests/__init__.py
+++ dinette/tests/__init__.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.test import Client
 from django import template
 from django.db.models import get_model
-
+from django.core.urlresolvers import reverse
 class Testmaker(TestCase):
     fixtures = ['test_data']
 
@@ -12,17 +12,24 @@ class Testmaker(TestCase):
         self.client = Client()
 
     def test_index_page(self):
-        r = self.client.get('/forum/', {})
+        """Check if the homepage is accessible by all"""
+        r = self.client.get(reverse('dinette_category'), {})
         self.assertEqual(r.status_code, 200)
+
+    def test_new_topics(self):
+        r = self.client.get(reverse('dinette_new_for_user'))
+        #check for redirection for guest users 
+        self.assertEqual(r.status_code,302)
+        #check for results in case of loggedin users
+        r = self.client.login(username='plaban',password='plaban') #this is in test fixture
+        self.assertEqual(r,True) 
+        r = self.client.get(reverse('dinette_new_for_user'))
+        self.assertEqual( len(r.context['new_topic_list']),0) # as there are no entry in the db
+
+    def test_unanswered_topics(self):
+        r = self.client.get(reverse('dinette_unanswered'))
+        print r.context['unanswered_topics']
+        
         
 
-           
-
-
-
-    
-
-
-    
-    
 
