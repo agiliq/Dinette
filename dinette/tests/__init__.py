5beb4a87bc78a5aee41625eace4e2cb82b9cadde
Plaban <plaban.nayak@gmail.com>
2011-09-29 11:23:14 +0530

    first successful test

--- dinette/tests/__init__.py
+++ dinette/tests/__init__.py
@@ -6,8 +6,7 @@ from django import template
 from django.db.models import get_model
 
 class Testmaker(TestCase):
-
-    fixtures = ["dinette_testmaker"]
+    fixtures = ['test_data']
 
     def setUp(self):
         self.client = Client()
@@ -15,16 +14,12 @@ class Testmaker(TestCase):
     def test_index_page(self):
         r = self.client.get('/forum/', {})
         self.assertEqual(r.status_code, 200)
+        
 
            
-    def test_forumpost_reply(self):
-        r = self.client.post('/forum/post/reply/', {'topicid': '7', 'message': 'Hello', 'authenticated': 'True', 'file': '', })
-        self.assertEqual(r.statuscode,200)
-    
 
-    def test_forumpost_topic(self):
-        r = self.client.post('/forum/post/topic/', {'message': '2', 'authenticated': 'True', 'categoryid': '7', 'file': '', 'subject': '2', })
-        self.assertEqual(r.status_code, 200)
+
+
     
 
 
