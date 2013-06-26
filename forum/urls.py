3c72b3a8af0d465c3d1d09329cf92db8f65d8d1c
Akshar Raaj <akshar@agiliq.com>
2013-06-26 18:34:55 +0530

    Remove cruft

--- forum/urls.py
+++ forum/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,11 +9,6 @@ urlpatterns = patterns('',
     (r'^forum/', include('dinette.urls')),
     (r'^accounts/', include('socialauth.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
 
